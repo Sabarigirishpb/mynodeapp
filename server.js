f782a88ed8419f4cead9cc6a83579146e7f1450d
Sabarigirishpb <111074927+Sabarigirishpb@users.noreply.github.com>
2022-09-05 01:23:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World- Node-app from sabari');
+  res.send('Hello World- Node-app from sabarigirish');
 });
 
 app.listen(PORT, HOST);
