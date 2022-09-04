aca9a4b47bda58023d7c478940139dd9a89f031e
sabari girish <sabarigirishpb@gmail.com>
2022-09-05 01:12:53 +0530

    edited server-index

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World- Node from sabari');
+  res.send('Hello World- Node-app from sabari');
 });
 
 app.listen(PORT, HOST);
